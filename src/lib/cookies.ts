b53cccb4e8ca4cd6a076c746d438ce0895cd5f83
JoaoAlmeid <jmalmeida700@gmail.com>
2025-07-08 19:08:58 -0300

    ajuste de dominio dos cookies

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -4,17 +4,19 @@ export function gerarCookiesDeAutenticacao(token: string, refreshToken: string) 
   const tokenCookie = serialize('token', token, {
     httpOnly: true,
     secure: true,
+    sameSite: 'none',
     maxAge: 60 * 60 * 24,
     path: '/',
-    sameSite: 'none',
+    domain: '.ccomfm.com.br'
   });
 
   const refreshTokenCookie = serialize('refreshToken', refreshToken, {
     httpOnly: true,
     secure: true,
+    sameSite: 'none',
     maxAge: 60 * 60 * 24 * 7,
     path: '/',
-    sameSite: 'none',
+    domain: '.ccomfm.com.br'
   });
 
   return [tokenCookie, refreshTokenCookie];
