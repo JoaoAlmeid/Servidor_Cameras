2c5b43e68f9a973ced50fad56470516ebf01e8ef
JoaoAlmeid <jmalmeida700@gmail.com>
2025-07-09 18:01:19 -0300

    Ajuste nas variaveis do pm2

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -10,6 +10,12 @@ module.exports = {
       max_memory_restart: '1024M',
       time: true,
       interpreter_args: '--max-old-space-size=1024',
+      env: {
+        NODE_ENV: 'production',
+        FRONTEND_URL: 'https://camera.ccomfm.com.br',
+        BACKEND_URL: 'https://zdxv152.ccomfm.com.br',
+        PORT: '3450'
+      }
     },
   ],
 };
